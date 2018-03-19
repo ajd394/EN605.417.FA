2ffc7add64ddf67dbb07f95107fcbfcd4686d826
Andrew DiPrinzio <andrew.diprinzio@jhuapl.edu>
2018-03-18 23:05:13 -0400

    comments and output

--- module5/run.sh
+++ module5/run.sh
@@ -6,15 +6,20 @@ FILENAME=assignment5_constant_mem
 
 nvcc $FILENAME.cu -lcudart -Wno-deprecated-gpu-targets -o $FILENAME 
 
+echo "Constant Memory Experiment"
+echo "**************************"
 if [[ ! -v CI ]]; then
     ./$FILENAME -t 4096 -b 32
-	./$FILENAME -t 8192 -b 64
+	./$FILENAME -t 99,968 -b 64
     ./$FILENAME -t 8192 -b 32
 fi
 
 FILENAME=assignment5_shared_mem
 
 nvcc $FILENAME.cu -lcudart -Wno-deprecated-gpu-targets -o $FILENAME 
+
+echo "Shared Memory Experiment"
+echo "**************************"
 
 if [[ ! -v CI ]]; then
     ./$FILENAME -t 32000000 -b 32
