2192568bcebda420009cd2230dd8b0189a95f8e3
Andrew DiPrinzio <andrew.diprinzio@jhuapl.edu>
2018-04-16 18:41:49 -0400

    npp cleanup

--- module9/assignment9_npp/assignment9_npp.cpp
+++ module9/assignment9_npp/assignment9_npp.cpp
@@ -30,29 +30,6 @@ Resources:
 #include <helper_string.h>
 #include <helper_cuda.h>
 
-// inline int cudaDeviceInit(int argc, const char **argv)
-// {
-//     int deviceCount;
-//     checkCudaErrors(cudaGetDeviceCount(&deviceCount));
-
-//     if (deviceCount == 0)
-//     {
-//         std::cerr << "CUDA error: no devices supporting CUDA." << std::endl;
-//         exit(EXIT_FAILURE);
-//     }
-
-//     int dev = findCudaDevice(argc, argv);
-
-//     cudaDeviceProp deviceProp;
-//     cudaGetDeviceProperties(&deviceProp, dev);
-//     std::cerr << "cudaSetDevice GPU" << dev << " = " << deviceProp.name << std::endl;
-
-//     checkCudaErrors(cudaSetDevice(dev));
-
-//     return dev;
-// }
-
-
 int main(int argc, char *argv[])
 {
     printf("%s Starting...\n\n", argv[0]);
