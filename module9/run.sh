aa8cecde5563e604f49f1042239e5ab1ceb081e2
Andrew DiPrinzio <andrew.diprinzio@jhuapl.edu>
2018-04-16 17:58:05 -0400

    init mod9 npp

---
+++ module9/run.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+set -e
+
+FILENAME=assignment9
+
+CURR_DIR=${FILENAME}_npp
+pushd ${CURR_DIR}
+    make
+
+    if [[ ! -v CI ]]; then
+        ./${CURR_DIR} --masksz 5
+        ./${CURR_DIR} --masksz 10
+
+        ./${CURR_DIR} --input ../../common/data/flower.ppm --masksz 5
+        ./${CURR_DIR} --input ../../common/data/flower.ppm --masksz 10
+    fi
+popd
+
+# CURR_DIR=${FILENAME}_nvgraph
+# pushd ${CURR_DIR}
+#     make
+
+#     if [[ ! -v CI ]]; then
+#         ./${CURR_DIR}
+#     fi
+# popd
